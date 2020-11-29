95765b1df14cd5685778cda2d4457d424821fbd6
cleara <clearasia_prastika_27rpl@student.smktelkom-mlg.sch.id>
2020-11-29 08:56:58 +0700

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>About page</h1>
+        This is About page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
